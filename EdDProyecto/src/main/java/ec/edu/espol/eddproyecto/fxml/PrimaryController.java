9663b810dd976ba4697abe7bbf336916bda9d484
PeterMiranda <pemiplua@espol.edu.ec>
2023-11-09 23:50:06 -0500

    PrimaryController and fxml

--- EdDProyecto/src/main/java/ec/edu/espol/eddproyecto/fxml/PrimaryController.java
+++ EdDProyecto/src/main/java/ec/edu/espol/eddproyecto/fxml/PrimaryController.java
@@ -2,11 +2,26 @@ package ec.edu.espol.eddproyecto.fxml;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
-
+    
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private ListView<String> tableView; // Especifica el tipo de elementos aquí
+    @FXML
+    private TextField searchText;
+
+
+    private void addItemsToListView() {
+        // Aquí puedes agregar elementos a tu ListView
+        tableView.getItems().addAll("Elemento 1", "Elemento 2", "Elemento 3");
+    }
+
+
+    // Método de inicialización de controlador de FXML
+    @FXML
+    private void initialize() {
+        addItemsToListView();
     }
 }
